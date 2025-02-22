2869aa5b1c87d2c245f9e8159bc5cccf038bf108
Andrii Mitnovych <10116550+formatBCE@users.noreply.github.com>
2025-02-22 15:41:26 -0800

    Revert "DFU OTA update implementation (#35)" (#36)
    This reverts commit 556fe920b9be63063cfdf430dab960f491db9e6e.

--- esphome/components/respeaker_lite/__init__.py
+++ esphome/components/respeaker_lite/__init__.py
@@ -1,142 +1,33 @@
-import hashlib
-from pathlib import Path
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome import pins
-from esphome import automation, core, external_files
 from esphome.components import i2c, binary_sensor, text_sensor
 from esphome.const import (
     CONF_ID,
-    CONF_ON_ERROR,
-    CONF_RAW_DATA_ID,
-    CONF_RESET_PIN,
-    CONF_TRIGGER_ID,
-    CONF_URL,
-    CONF_VERSION,
+    CONF_RESET_PIN
 )
-from esphome.core import HexInt
+from esphome import pins
+
+MULTI_CONF = True
 
 AUTO_LOAD = [ "binary_sensor", "text_sensor" ]
-CODEOWNERS = ["@formatBCE"]
-DEPENDENCIES = ["i2c"]
+
+DEPENDENCIES = ['i2c']
+
+respeaker_lite_ns = cg.esphome_ns.namespace('respeaker_lite')
+RespeakerLite = respeaker_lite_ns.class_('RespeakerLite', i2c.I2CDevice, cg.Component)
 
 CONF_MUTE_STATE= "mute_state"
 CONF_FIRMWARE_VERSION= "firmware_version"
-CONF_FIRMWARE = "firmware"
-CONF_MD5 = "md5"
-CONF_ON_BEGIN = "on_begin"
-CONF_ON_END = "on_end"
-CONF_ON_PROGRESS = "on_progress"
+DEFAULT_ADDRESS = 0x42
 
-DOMAIN = "respeaker_lite"
-
-respeaker_lite_ns = cg.esphome_ns.namespace("respeaker_lite")
-RespeakerLite = respeaker_lite_ns.class_("RespeakerLite", i2c.I2CDevice, cg.Component)
-RespeakerLiteFlashAction = respeaker_lite_ns.class_("RespeakerLiteFlashAction", automation.Action)
-
-DFUEndTrigger = respeaker_lite_ns.class_("DFUEndTrigger", automation.Trigger.template())
-DFUErrorTrigger = respeaker_lite_ns.class_("DFUErrorTrigger", automation.Trigger.template())
-DFUProgressTrigger = respeaker_lite_ns.class_(
-    "DFUProgressTrigger", automation.Trigger.template()
-)
-DFUStartTrigger = respeaker_lite_ns.class_("DFUStartTrigger", automation.Trigger.template())
-
-
-def _compute_local_file_path(url: str) -> Path:
-    h = hashlib.new("sha256")
-    h.update(url.encode())
-    key = h.hexdigest()[:8]
-    base_dir = external_files.compute_local_file_dir(DOMAIN)
-    return base_dir / key
-
-
-def download_firmware(config):
-    url = config[CONF_URL]
-    path = _compute_local_file_path(url)
-    external_files.download_content(url, path)
-
-    try:
-        with open(path, "r+b") as f:
-            firmware_bin = f.read()
-    except FileNotFoundError as e:
-        raise cv.Invalid(f"Could not open firmware file {path}: {e}") from e
-
-    firmware_bin_md5 = hashlib.md5(firmware_bin).hexdigest()
-    if firmware_bin_md5 != config[CONF_MD5]:
-        raise cv.Invalid(f"{CONF_MD5} is not consistent with file contents")
-
-    return config
-
-
-CONFIG_SCHEMA = (
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.declare_id(RespeakerLite),
-            cv.Required(CONF_RESET_PIN): pins.gpio_output_pin_schema,
-            cv.Optional(CONF_MUTE_STATE): binary_sensor.binary_sensor_schema(),
-            cv.Optional(CONF_FIRMWARE_VERSION): text_sensor.text_sensor_schema(),
-            cv.GenerateID(CONF_RAW_DATA_ID): cv.declare_id(cg.uint8),
-            cv.Optional(CONF_FIRMWARE): cv.All(
-                {
-                    cv.Required(CONF_URL): cv.url,
-                    cv.Required(CONF_VERSION): cv.version_number,
-                    cv.Required(CONF_MD5): cv.All(cv.string, cv.Length(min=32, max=32)),
-                    cv.Optional(CONF_ON_BEGIN): automation.validate_automation(
-                        {
-                            cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(
-                                DFUStartTrigger
-                            ),
-                        }
-                    ),
-                    cv.Optional(CONF_ON_END): automation.validate_automation(
-                        {
-                            cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(
-                                DFUEndTrigger
-                            ),
-                        }
-                    ),
-                    cv.Optional(CONF_ON_ERROR): automation.validate_automation(
-                        {
-                            cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(
-                                DFUErrorTrigger
-                            ),
-                        }
-                    ),
-                    cv.Optional(CONF_ON_PROGRESS): automation.validate_automation(
-                        {
-                            cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(
-                                DFUProgressTrigger
-                            ),
-                        }
-                    ),
-                },
-                download_firmware,
-            ),
-        }
-    )
-    .extend(cv.COMPONENT_SCHEMA)
-    .extend(i2c.i2c_device_schema(0x42))
-)
-
-
-OTA_RESPEAKER_LITE_FLASH_ACTION_SCHEMA = cv.Schema(
+CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend(
     {
-        cv.GenerateID(): cv.use_id(RespeakerLite),
+        cv.GenerateID(): cv.declare_id(RespeakerLite),
+        cv.Required(CONF_RESET_PIN): pins.gpio_output_pin_schema,
+        cv.Optional(CONF_MUTE_STATE): binary_sensor.binary_sensor_schema(),
+        cv.Optional(CONF_FIRMWARE_VERSION): text_sensor.text_sensor_schema(),
     }
-)
-
-
-@automation.register_action(
-    "respeaker_lite.flash",
-    RespeakerLiteFlashAction,
-    OTA_RESPEAKER_LITE_FLASH_ACTION_SCHEMA,
-)
-async def respeaker_lite_flash_action_to_code(config, action_id, template_arg, args):
-    paren = await cg.get_variable(config[CONF_ID])
-    var = cg.new_Pvariable(action_id, template_arg, paren)
-
-    return var
-
+).extend(i2c.i2c_device_schema(DEFAULT_ADDRESS))
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
@@ -152,46 +43,3 @@ async def to_code(config):
     if CONF_FIRMWARE_VERSION in config:
         firmware_version = await text_sensor.new_text_sensor(config[CONF_FIRMWARE_VERSION])
         cg.add(var.set_firmware_version(firmware_version))
-
-    if config_fw := config.get(CONF_FIRMWARE):
-        firmware_version = config_fw[CONF_VERSION].split(".")
-        path = _compute_local_file_path(config_fw[CONF_URL])
-
-        try:
-            with open(path, "r+b") as f:
-                firmware_bin = f.read()
-        except FileNotFoundError as e:
-            raise core.EsphomeError(f"Could not open firmware file {path}: {e}")
-
-        # Convert retrieved binary file to an array of ints
-        rhs = [HexInt(x) for x in firmware_bin]
-        # Create an array which will reside in program memory and set the pointer to it
-        firmware_bin_arr = cg.progmem_array(config[CONF_RAW_DATA_ID], rhs)
-        cg.add(var.set_firmware_bin(firmware_bin_arr, len(rhs)))
-        cg.add(
-            var.set_firmware_version(
-                int(firmware_version[0]),
-                int(firmware_version[1]),
-                int(firmware_version[2]),
-            )
-        )
-
-        use_state_callback = False
-        for conf in config_fw.get(CONF_ON_BEGIN, []):
-            trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
-            await automation.build_automation(trigger, [], conf)
-            use_state_callback = True
-        for conf in config_fw.get(CONF_ON_PROGRESS, []):
-            trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
-            await automation.build_automation(trigger, [(float, "x")], conf)
-            use_state_callback = True
-        for conf in config_fw.get(CONF_ON_END, []):
-            trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
-            await automation.build_automation(trigger, [], conf)
-            use_state_callback = True
-        for conf in config_fw.get(CONF_ON_ERROR, []):
-            trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
-            await automation.build_automation(trigger, [(cg.uint8, "x")], conf)
-            use_state_callback = True
-        if use_state_callback:
-            cg.add_define("USE_RESPEAKER_LITE_STATE_CALLBACK")
